3bb0cd3a0e8942b6875894e9d9c421c83e236ac8
yhx <2045399856@qq.com>
2024-07-18 16:45:45 +0800

    add:添加commitlint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,45 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
+/*
+ * @Author: yhx 2045399856@qq.com
+ * @Date: 2024-01-26 14:50:05
+ * @LastEditTime: 2024-04-03 15:39:03
+ * @FilePath: \hagh-frontend\.eslintrc.cjs
+ * @Description:
+ *
+ */
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+  env: {
+    browser: true
+  },
+  extends: ['eslint:recommended', 'airbnb'],
+  // "extends": ['eslint-config-ali'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+    semi: 'error',
+    'no-console': 'off',
+    'no-debugger': 'off'
+  },
+  ignorePatterns: ['node_modules/', 'build/', 'dist/'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']]
+      }
+    }
   }
 }
