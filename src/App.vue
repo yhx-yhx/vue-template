45e37b06bda884d98b11e595fb99e1ed19b2b545
yhx <2045399856@qq.com>
2024-07-18 14:04:54 +0800

    add:init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="conatainer">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.conatainer {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    home
+  </main>
+</template>
