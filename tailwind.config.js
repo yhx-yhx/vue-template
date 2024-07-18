9455861f927d7757503be9701576ff014fece208
yhx <2045399856@qq.com>
2024-07-18 14:45:43 +0800

    add:iconify

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
+import { addDynamicIconSelectors } from '@iconify/tailwind'
 /** @type {import('tailwindcss').Config} */
 export default {
- content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: [addDynamicIconSelectors()],
+  darkMode: ['selector', '[data-mode="dark"]']
 }
-
