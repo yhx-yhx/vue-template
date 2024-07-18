cd0acdc84b66eba19988112ca2a9b921ed993abc
yhx <2045399856@qq.com>
2024-07-18 14:47:13 +0800

    add:autoprefixer 自动添加css前缀

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 export default {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {
+      overrideBrowserslist: [
+        'Chrome > 68',
+        'ff > 31',
+        'last 10 versions' // 所有主流浏览器最近10版本用
+      ],
+      grid: true
+    }
+  }
 }
