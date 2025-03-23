2181c7962d087edd8222bf373a07bc5a3503f314
yhx <2045399856@qq.com>
2025-03-23 17:34:29 +0800

    chore: upgrade ESLint and related dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,12 @@
-import airbnb from 'eslint-config-airbnb'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
 
-// 创建一个清理后的 globals 对象
-const cleanGlobals = Object.fromEntries(
-  Object.entries({
-    ...globals.browser,
-    ...globals.es2021,
-    ...globals.node
-  }).map(([key, value]) => [key.trim(), value])
-)
-
-export default [
-  {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-    ignores: ['dist/**', 'node_modules/**', 'src/auto-imports.d.ts', 'src/components.d.ts'],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: 'module',
-      globals: cleanGlobals,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
-    },
-    plugins: {
-      ...airbnb.plugins
-    },
-    rules: {
-      ...airbnb.rules,
-      'import/no-extraneous-dependencies': 'off',
-      'import/prefer-default-export': 'off',
-      'no-param-reassign': 'off',
-      'no-unused-vars': 'warn'
-    }
-  }
-]
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { files: ['**/*.{js,mjs,cjs,vue}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,vue}'], plugins: { js }, extends: ['js/recommended'] },
+  pluginVue.configs['flat/essential']
+])
